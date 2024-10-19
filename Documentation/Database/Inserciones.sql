0e8f078759c453080b678ddf17195235da3d5141
rmichelv <richard.villalba.mendieta@gmail.com>
2024-10-19 01:30:39 -0400

    agregando el proyecto con inicio y validaciones

---
+++ Documentation/Database/Inserciones.sql
@@ -0,0 +1,35 @@
+INSERT INTO nationalities (name) VALUES
+('Boliviana'),
+('Argentina'),
+('Chilena'),
+('Colombiana'),
+('Peruana'),
+('Ecuatoriana'),
+('Venezolana'),
+('Brasileña'),
+('Uruguaya'),
+('Paraguaya'),
+('Mexicana'),
+('Cubana'),
+('Española'),
+('Francesa'),
+('Italiana'),
+('Alemana'),
+('Portuguesa'),
+('Canadiense'),
+('Estadounidense'),
+('Japonesa');
+
+
+INSERT INTO rols (name) VALUES
+('Administrador'),
+('Recepcionista'),
+('Cliente'),
+('Mantenimiento'),
+('Limpieza'),
+('Gerente'),
+('Cocinero'),
+('Seguridad'),
+('Contador'),
+('Supervisor');
+
